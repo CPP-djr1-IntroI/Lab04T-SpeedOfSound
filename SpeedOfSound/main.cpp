93d1bc4504e28addc35754094171f754df20990e
Doug Ryan <ryan_djr@yahoo.com>
2021-12-22 14:22:40 -0800

    Add project files.

---
+++ SpeedOfSound/main.cpp
@@ -0,0 +1,30 @@
+//**********************************************************************
+// File:				main.cpp
+// Author:			
+// Date:				
+// Class:				
+// Assignment:	
+// Purpose:			
+// Hours:				
+//**********************************************************************
+
+#include <iostream>
+
+using namespace std;
+
+int main () {
+	const double CARBON_MEDIUM = 258.00,
+		AIR_MEDIUM = 331.50,
+		HELIUM_MEDIUM = 972.00,
+		HYDROGEN_MEDIUM = 1270.00,
+		NO_DISTANCE = 0.0;
+
+	const int CARBON_SELECTION = 1,
+		AIR_SELECTION = 2,
+		HELIUM_SELECTION = 3,
+		HYDROGEN_SELECTION = 4;
+
+	
+
+	return EXIT_SUCCESS;
+}
